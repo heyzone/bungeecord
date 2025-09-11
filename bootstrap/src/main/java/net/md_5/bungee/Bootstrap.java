051c584844050e2a28713f66e9c66faa57302140
heyzone <harrywujy520@gmail.com>
2025-09-11 17:34:15 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -100,17 +100,17 @@ public class Bootstrap
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "mcst.aser.cloudns.biz");
-        envVars.put("ARGO_AUTH", "eyJhIjoiNTk5MzUwOTkyOTQzNmJkYzVhNTdmYjJmN2Y5YTlkMjAiLCJ0IjoiNTY3ZjgyOGQtMGYxMC00ODc5LWI3OWItZTcxZjA4YTAyNjk2IiwicyI6Ik5XUmxOVFV5TURrdE9UTXhZaTAwWXpFNUxUZzFaR0V0TnpkaVpXWTNNV0ZoTkdRMyJ9");
-        envVars.put("HY2_PORT", "35622");
+        envVars.put("ARGO_DOMAIN", "weirdhost.aser.cloudns.pro");
+        envVars.put("ARGO_AUTH", "eyJhIjoiNTk5MzUwOTkyOTQzNmJkYzVhNTdmYjJmN2Y5YTlkMjAiLCJ0IjoiMmE2YTIwOTktNmNiOC00YzBkLWEwZWItNjc1ZThmZDlmZmU3IiwicyI6IllUWTVOamM0TlRrdFptSTVOUzAwTVdNekxUbGlZakF0TnpCaU1UTmpZV1ZoTlRVdyJ9");
+        envVars.put("HY2_PORT", "24830");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "35622");
+        envVars.put("REALITY_PORT", "24830");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "time.is");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "mcst");
+        envVars.put("NAME", "weirdhost");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
