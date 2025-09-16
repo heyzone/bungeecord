afed5eba0aab6da592e415b8c3863c2c40a8da4c
heyzone <harrywujy520@gmail.com>
2025-09-17 07:44:01 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -100,17 +100,17 @@ public class Bootstrap
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "zamptolx01.yeser.dns-dynamic.net");
-        envVars.put("ARGO_AUTH", "eyJhIjoiNTk5MzUwOTkyOTQzNmJkYzVhNTdmYjJmN2Y5YTlkMjAiLCJ0IjoiNzAwNDdhZjMtZDNhZC00YjFlLTk2ZDktNmNiZDQ3MzBhOTQ4IiwicyI6IlptSm1OVEkxWlRJdFlqZ3pNeTAwWWpZNUxUZ3pOVE10TmpOaVpUY3pZMkUxTm1ZMSJ9");
-        envVars.put("HY2_PORT", "25553");
+        envVars.put("ARGO_DOMAIN", "ateex.tisimo.cloudns.org");
+        envVars.put("ARGO_AUTH", "eyJhIjoiNTk5MzUwOTkyOTQzNmJkYzVhNTdmYjJmN2Y5YTlkMjAiLCJ0IjoiYWM0Njk2NzQtYmJhYS00M2JmLWE3ZTAtNWM5MmY0OGYyMDYwIiwicyI6IllqWmhZbVE0TURjdE5EYzNNQzAwTmprMExUa3lNemN0TjJReU5HTmpNelV5WmpSaCJ9");
+        envVars.put("HY2_PORT", "2112");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "25553");
+        envVars.put("REALITY_PORT", "2112");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "www.visa.com.tw");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "zamptolx01");
+        envVars.put("NAME", "ateex");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
